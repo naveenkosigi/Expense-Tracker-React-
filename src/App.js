f98bdedeac5a7d7dc17765b15eb29e338a92e7ea
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-21 14:09:54 +0530

    added expense form component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import ExpenseItem  from './components/ExpenseItem/ExpenseItem';
 import Card from './components/CardWrapper/CardComponent';
+import { ExpenseForm } from './components/ExpenseForm/ExpenseForm';
 
 function App() {
   return (
-    <Card className="expenses-container">
+    <div>
+      <ExpenseForm />
+      <Card className="expenses-container">
       <ExpenseItem title="1st Expense" price="23" date={new Date()}></ExpenseItem>
       <ExpenseItem title="1st Expense" price="100" date={new Date()}></ExpenseItem>
       <ExpenseItem title="1st Expense" price="280" date={new Date()}></ExpenseItem>
     </Card>
+    </div>
+    
   );
 }
 
