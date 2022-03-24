44616c7144b1e7c5fe137c98c0c1ff0e22a45f28
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-24 20:51:42 +0530

    added 2 way data-binding to expenseForm

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -9,39 +9,52 @@ export const ExpenseForm = () => {
         date:''
     });
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        /*setState({
-            title:event.target.querySelector("#expenseTitle").value,
-            amount:event.target.querySelector("#expenseAmount").value,
-            date:event.target.querySelector("#expenseDate").value
-        });*/
-
-        setState( (prevState) => {
-            return{
+    const onExpenseTitleChange = (event) => {
+        setState((prevState) => {
+            return {
                 ...prevState,
-                title:event.target.querySelector("#expenseTitle").value,
-                amount:event.target.querySelector("#expenseAmount").value,
-                date:event.target.querySelector("#expenseDate").value
-            }
+                title:event.target.value
+            };
         });
+    };
+
+    const onExpenseAmountChange = (event) => {
+        setState((prevState) => {
+            return {
+                ...prevState,
+                amount:event.target.value
+            };
+        });
+    };
+
+    const onExpenseDateChange = (event) => {
+        setState((prevState) => {
+            return {
+                ...prevState,
+                date:event.target.value
+            };
+        });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
         console.log(state);
     };
 
     return (
-        <form className="new-expense" onSubmit={onSubmit}>
+        <form className="new-expense" onSubmit={onFormSubmit}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="expenseTitle">Title</label>
-                    <input type="text" id="expenseTitle"/>
+                    <input type="text" id="expenseTitle" value={state.title} onChange={onExpenseTitleChange}/>
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="expenseAmount">Amount</label>
-                    <input type="number" min="0.5" step="0.5" id="expenseAmount"/> 
+                    <input type="number" min="0.5" step="0.5" id="expenseAmount" value={state.amount} onChange={onExpenseAmountChange}/> 
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="expenseDate">Date</label>
-                    <input type="date" min="2022-01-01" max="2022-12-31" id="expenseDate"/> 
+                    <input type="date" min="2022-01-01" max="2022-12-31" id="expenseDate" value={state.date} onChange={onExpenseDateChange}/> 
                 </div>
             </div>
             <div className='new-expense__actions'>
