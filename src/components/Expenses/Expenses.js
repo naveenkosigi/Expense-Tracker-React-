847a064ab51530953d99e8a44b0983f2c604d008
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-24 23:58:14 +0530

    made expenses items dynamic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,18 +6,37 @@ import ExpenseItem from "../ExpenseItem/ExpenseItem";
 
 export const Expenses = (props) => {
 
-    const [selectedYear,setYear] = useState('2022')
+    const [selectedYear,setYear] = useState('2022');
+
+    const [expenses,setExpenses] = useState([
+        {
+            title:'1st Expense',
+            price:'23',
+            date:new Date()
+        },
+        {
+            title:'1st Expense',
+            price:'100',
+            date:new Date()
+        },
+        {
+            title:'1st Expense',
+            price:'80',
+            date:new Date()
+        }
+    ]);
 
     const onYearChange = (year) => {
         setYear(year);
     };
+
     
     return (
         <Card className="expenses-container">
-            <ExpenseDateFilter date={selectedYear} onYearChange={onYearChange}></ExpenseDateFilter> 
-            <ExpenseItem title="1st Expense" price="23" date={new Date()}></ExpenseItem>
-            <ExpenseItem title="1st Expense" price="100" date={new Date()}></ExpenseItem>
-            <ExpenseItem title="1st Expense" price="280" date={new Date()}></ExpenseItem>
+            <ExpenseDateFilter date={selectedYear} onYearChange={onYearChange}></ExpenseDateFilter>
+            {expenses.map((expense) => {
+                return (<ExpenseItem title={expense.title} price={expense.price} date={expense.date} />)
+            })} 
         </Card>
     );
 };
