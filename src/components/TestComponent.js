0d708ce47b30a51072f36abae1ad6ba5eebd3d42
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-25 16:05:36 +0530

    reconstructed the test component for better css

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -2,29 +2,12 @@ import './TestComponent.css';
 
 export const Test = () => {
     return(
-        <div className="expenses-container chart-container">
-            <div className="chart">
-                <span className="data-filler"/>
-            </div>
-            <div className="chart">
-                <span className="data-filler"/>
-            </div>
-            <div className="chart">
-                <span className="data-filler"/>
-            </div>
-            <div className="chart">
-                <span className="data-filler"/>
-            </div>
-            <div className="chart">
-                <span className="data-filler"/>
-            </div><div className="chart">
-                <span className="data-filler"/>
-            </div><div className="chart">
-                <span className="data-filler"/>
-            </div><div className="chart">
-                <span className="data-filler"/>
-            </div><div className="chart">
-                <span className="data-filler"/>
+        <div className='chart-listview-container expenses-container'>
+            <div className='chart-container'>
+                <span className='chart-label'>January</span>
+                <div className='data-container'>
+                    <span className='data-filler' />
+                </div>
             </div>
         </div>
     );
