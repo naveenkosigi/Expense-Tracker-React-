382aa84ce921018b7ed1125525e53126556e2b80
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-25 13:33:44 +0530

    added css to Expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,10 +2,11 @@
 import Card from "../CardWrapper/CardComponent";
 import { ExpenseDateFilter } from "../ExpenseDateFilter/ExpenseDateFilter";
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import './Expenses.css';
 
 export const Expenses = (props) => {
 
-    let expenseContent = <p>No Expenses Found</p>;
+    let expenseContent = <p class="no-expenses">No Expenses Found</p>;
     const filteredExpenses=props.expenses.filter((expense) => 
                                 expense.date.getFullYear().toString() === props.defaultYear
                            );
