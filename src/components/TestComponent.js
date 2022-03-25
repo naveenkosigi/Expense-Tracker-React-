111e03571646bec529f2dfb22e1b7c6f39d846bc
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-25 16:09:54 +0530

    added basic ui for more than one month

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -9,6 +9,18 @@ export const Test = () => {
                     <span className='data-filler' />
                 </div>
             </div>
+            <div className='chart-container'>
+                <span className='chart-label'>January</span>
+                <div className='data-container'>
+                    <span className='data-filler' />
+                </div>
+            </div>
+            <div className='chart-container'>
+                <span className='chart-label'>January</span>
+                <div className='data-container'>
+                    <span className='data-filler' />
+                </div>
+            </div>
         </div>
     );
 }
