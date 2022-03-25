c20fb968704c29b30bfd14e61ab0a9949b2290d8
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-03-25 13:24:51 +0530

    added conditional content in expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,16 +5,22 @@ import ExpenseItem from "../ExpenseItem/ExpenseItem";
 
 export const Expenses = (props) => {
 
+    let expenseContent = <p>No Expenses Found</p>;
+    const filteredExpenses=props.expenses.filter((expense) => 
+                                expense.date.getFullYear().toString() === props.defaultYear
+                           );
+
+
+    if(filteredExpenses.length > 0){
+        expenseContent = filteredExpenses.map((expense) => 
+                                (<ExpenseItem key={expense.id} title={expense.title} price={expense.price} date={expense.date} />)
+                            );
+    }
     
     return (
         <Card className="expenses-container">
             <ExpenseDateFilter date={props.defaultYear} onYearChange={props.onYearChange}></ExpenseDateFilter>
-            {props.expenses.filter((expense) => 
-                expense.date.getFullYear().toString() === props.defaultYear
-            ).map((expense) => 
-                (<ExpenseItem key={expense.id} title={expense.title} price={expense.price} date={expense.date} />)
-            )
-            } 
+            {expenseContent} 
         </Card>
     );
 };
